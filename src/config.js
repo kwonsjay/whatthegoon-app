1069f63f8f3465df21651ea4191400c2eec7bfd5
Soon Jae Kwon <kwonsjay@gmail.com>
2019-10-17 11:24:08 -0400

    set up react

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "whatthegoon-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ttzncalsae.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_uA0JhRHD0",
+    APP_CLIENT_ID: "4ofj2ulamq2jcc09eg9o3tsmgi",
+    IDENTITY_POOL_ID: "us-east-1:a1051dd8-1dd9-4d8b-bbed-ab9322fe61bd"
+  }
+};
